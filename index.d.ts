6c41720343def445756898c4e3e3ce56bc7f9e26
Thierry <thierry@byscripts.info>
2025-07-30 09:04:01 +0200

    fix build error

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 declare module 'ndjson-readablestream' {
-  export default function <T = any>(readableStream: ReadableStream): AsyncGenerator<T>;
+  export default function readNDJSONStream<T = any>(readableStream: ReadableStream): AsyncGenerator<T>;
 }
